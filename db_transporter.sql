9a0ea12c5ff2418a88b46466380ab3c11643487e
Rahmat Raditya <48813816+RahmatRaditya@users.noreply.github.com>
2023-05-17 21:20:04 +0700

    Add data dummy

---
+++ db_transporter.sql
@@ -0,0 +1,241 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 17 Bulan Mei 2023 pada 16.18
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_transporter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `kode_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(200) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `stok_barang` int(11) DEFAULT NULL,
+  `harga_barang` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `deskripsi`, `stok_barang`, `harga_barang`, `created_at`, `updated_at`) VALUES
+(3, 'B009', 'Helm888', 'Helm', 75, 190000, '2023-05-15 14:27:29', '2023-05-15 14:27:29'),
+(4, 'B005', 'Sepatu', 'Sepatu', 90, 450000, '2023-05-17 05:27:45', '2023-05-17 05:27:45'),
+(5, 'B0088', 'Keyboard Mechacnical', 'Keyboard', 280, 250000, '2023-05-17 13:48:29', '2023-05-17 13:51:35');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lokasi`
+--
+
+CREATE TABLE `lokasi` (
+  `id` int(11) NOT NULL,
+  `kode_lokasi` varchar(10) DEFAULT NULL,
+  `nama_lokasi` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `lokasi`
+--
+
+INSERT INTO `lokasi` (`id`, `kode_lokasi`, `nama_lokasi`, `created_at`, `updated_at`) VALUES
+(1, 'L004', 'Pasar Senen sabtu minggu', '2023-05-15 14:59:23', '2023-05-15 15:06:23'),
+(3, 'L005', 'Pasar Senen', '2023-05-15 15:00:05', '2023-05-15 15:00:05'),
+(4, 'L001', 'Kebayoran Baru New', '2023-05-17 13:57:35', '2023-05-17 13:59:22');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengiriman`
+--
+
+CREATE TABLE `pengiriman` (
+  `id` int(11) NOT NULL,
+  `no_pengiriman` varchar(15) NOT NULL,
+  `tanggal` date NOT NULL,
+  `lokasi_id` int(11) DEFAULT NULL,
+  `barang_id` int(11) DEFAULT NULL,
+  `jumlah_barang` int(11) NOT NULL,
+  `harga_barang` int(11) DEFAULT NULL,
+  `kurir_id` int(11) DEFAULT NULL,
+  `is_approved` tinyint(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `pengiriman`
+--
+
+INSERT INTO `pengiriman` (`id`, `no_pengiriman`, `tanggal`, `lokasi_id`, `barang_id`, `jumlah_barang`, `harga_barang`, `kurir_id`, `is_approved`, `created_at`, `updated_at`) VALUES
+(2, 'P0002', '2023-05-15', 2, 2, 6, 50000, 2, 0, '2023-05-15 15:52:49', '2023-05-15 15:52:49'),
+(3, 'P0003', '2023-05-15', 2, 2, 6, 15000, 2, 1, '2023-05-15 16:00:01', '2023-05-15 16:00:01'),
+(4, 'P0004', '2023-05-15', 2, 2, 6, 32000, 2, 0, '2023-05-15 16:00:33', '2023-05-15 16:00:33'),
+(7, 'P0005', '2023-05-15', 4, 5, 1, 450000, 2, 1, '2023-05-17 05:30:14', '2023-05-17 05:35:08'),
+(8, 'P0007', '2023-05-20', 3, 4, 2, 450000, 4, 1, '2023-05-17 14:04:22', '2023-05-17 14:07:44');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Rahmat Raditya', '411201054@mahasiswa.undira.ac.id', NULL, '$2y$10$N7bpJeT.kAtdIc2PUoA5gON8z83E7sJuTy9Gak5Z5o5/lv9o/4zeW', NULL, '2023-05-15 06:45:30', '2023-05-15 06:45:30'),
+(2, 'kurir1', 'kurir1@mail.com', NULL, '123456', NULL, NULL, NULL),
+(3, 'kurir2', 'kurir2@mail.com', NULL, '123456', NULL, NULL, NULL),
+(4, 'Rahmat Raditya', 'rahmat@mail.com', NULL, '$2y$10$0wqEAcRnfpiPhzwd0ncF/uAe/0NgToQ18ctC1ElGT.bcConJ5Xis2', NULL, '2023-05-17 06:45:23', '2023-05-17 06:45:23');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `lokasi`
+--
+ALTER TABLE `lokasi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indeks untuk tabel `pengiriman`
+--
+ALTER TABLE `pengiriman`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `lokasi`
+--
+ALTER TABLE `lokasi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengiriman`
+--
+ALTER TABLE `pengiriman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
